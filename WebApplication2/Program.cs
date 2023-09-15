42c2868e939bdf0f8a5d442c838cb0d145723e71
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-15 20:05:05 +0300

    task1

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,33 +1,49 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.VisualBasic;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder();
+var services = builder.Services;
+services.AddTransient<ICalcService, CalcService>();
 var app = builder.Build();
-builder.Configuration
-   .AddJsonFile("config.json")
-   .AddJsonFile("config2.json")
-   .AddXmlFile("config.xml")
-   .AddIniFile("config.ini");
-app.MapGet("/", (IConfiguration appConfig) => {
-    var appleemployees = int.Parse(appConfig["apple:employees"]);
-    var googleemployees = int.Parse(appConfig["Google-employees-number"]);
-    var microsoftemployees = int.Parse(appConfig["Microsoft-employees-number"]);
-    var mydata = $"\n\nname:{appConfig["name"]}, surname:{appConfig["surname"]}";
-    if (appleemployees > microsoftemployees)
-    {
-        if (appleemployees > googleemployees)
-        {
-            return $"apple:{appleemployees}" + mydata;
-        }
-    }
-    else if (googleemployees > microsoftemployees)
-    {
-        return $"google:{googleemployees}" + mydata;
-    }
-    else {
-        return $"microsoft:{microsoftemployees}" + mydata;
-    }
-    return "";
-    });
+
+app.Run(async context =>
+{
+    var calcService = app.Services.GetService<ICalcService>();
+    var sb = new StringBuilder();
+    sb.Append($"22 + 33: {calcService?.Add(22, 33)}\n");
+    sb.Append($"23 - 6: {calcService?.Subtract(23, 6)}\n");
+    sb.Append($"5 * 6: {calcService?.Mylt(5, 6)}\n");
+    sb.Append($"5 / 6: {calcService?.Divide(5.0, 6.0)}\n");
+    await context.Response.WriteAsync(sb.ToString());
+});
+
 app.Run();
 
+interface ICalcService { 
+    T Add<T>(T a, T b);
+    T Subtract<T>(T a, T b);
+    T Mylt<T>(T a, T b);
+    T Divide<T>(T a, T b);
+}
+
+class CalcService : ICalcService
+{
+    public T Add<T>(T a, T b)
+    {
+        return (dynamic)a + (dynamic)b;
+    }
+
+    public T Divide<T>(T a, T b)
+    {
+        return (dynamic)a / (dynamic)b;
+    }
+
+    public T Mylt<T>(T a, T b)
+    {
+        return (dynamic)a * (dynamic)b;
+    }
+
+    public T Subtract<T>(T a, T b)
+    {
+        return (dynamic)a - (dynamic)b;
+    }
+}
