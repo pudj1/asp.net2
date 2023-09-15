b943d357afafa1759a179ff87854e61e10121cf4
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-09-15 22:29:41 +0300

    done

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -2,17 +2,21 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder();
 var services = builder.Services;
-services.AddTransient<ICalcService, CalcService>();
+services.AddSingleton<ICalcService, CalcService>();
+services.AddSingleton<ITimeService, TimeService>();
 var app = builder.Build();
 
 app.Run(async context =>
 {
+    context.Response.ContentType = "text/html; charset=utf-8";
     var calcService = app.Services.GetService<ICalcService>();
+    var timeService = app.Services.GetService<ITimeService>();
     var sb = new StringBuilder();
-    sb.Append($"22 + 33: {calcService?.Add(22, 33)}\n");
-    sb.Append($"23 - 6: {calcService?.Subtract(23, 6)}\n");
-    sb.Append($"5 * 6: {calcService?.Mylt(5, 6)}\n");
-    sb.Append($"5 / 6: {calcService?.Divide(5.0, 6.0)}\n");
+    sb.Append($"22 + 33: {calcService?.Add(22, 33)}<br>");
+    sb.Append($"23 - 6: {calcService?.Subtract(23, 6)}<br>");
+    sb.Append($"5 * 6: {calcService?.Mylt(5, 6)}<br>");
+    sb.Append($"5 / 6: {calcService?.Divide(5.0, 6.0)}<br>");
+    sb.Append($"{timeService.GetTime()}");
     await context.Response.WriteAsync(sb.ToString());
 });
 
@@ -45,5 +49,38 @@ class CalcService : ICalcService
     public T Subtract<T>(T a, T b)
     {
         return (dynamic)a - (dynamic)b;
+    }
+}
+
+interface ITimeService
+{
+    string GetTime();
+}
+
+class TimeService : ITimeService
+{
+    public string GetTime()
+    {
+        DateTime currentTime = DateTime.Now;
+
+        string timeOfDay;
+        if (currentTime.Hour >= 12 && currentTime.Hour < 18)
+        {
+            timeOfDay = "зараз день";
+        }
+        else if (currentTime.Hour >= 18 && currentTime.Hour < 24)
+        {
+            timeOfDay = "зараз вечір";
+        }
+        else if (currentTime.Hour >= 0 && currentTime.Hour < 6)
+        {
+            timeOfDay = "зараз ніч";
+        }
+        else
+        {
+            timeOfDay = "зараз ранок";
+        }
+
+        return timeOfDay;
     }
 }
