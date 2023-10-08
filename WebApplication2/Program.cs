9e6fd35f77a251edebcd8b5fbfdc4f1a0532e309
BUILTIN\Administrators <dxxdimaxxd228@gmail.com>
2023-10-08 18:53:50 +0300

    done

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,86 +1,76 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+using System.Net;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder();
-var services = builder.Services;
-services.AddSingleton<ICalcService, CalcService>();
-services.AddSingleton<ITimeService, TimeService>();
-var app = builder.Build();
-
-app.Run(async context =>
+internal class Program
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
-    var calcService = app.Services.GetService<ICalcService>();
-    var timeService = app.Services.GetService<ITimeService>();
-    var sb = new StringBuilder();
-    sb.Append($"22 + 33: {calcService?.Add(22, 33)}<br>");
-    sb.Append($"23 - 6: {calcService?.Subtract(23, 6)}<br>");
-    sb.Append($"5 * 6: {calcService?.Mylt(5, 6)}<br>");
-    sb.Append($"5 / 6: {calcService?.Divide(5.0, 6.0)}<br>");
-    sb.Append($"{timeService.GetTime()}");
-    await context.Response.WriteAsync(sb.ToString());
-});
-
-app.Run();
-
-interface ICalcService { 
-    T Add<T>(T a, T b);
-    T Subtract<T>(T a, T b);
-    T Mylt<T>(T a, T b);
-    T Divide<T>(T a, T b);
-}
-
-class CalcService : ICalcService
-{
-    public T Add<T>(T a, T b)
+    private static void Main(string[] args)
     {
-        return (dynamic)a + (dynamic)b;
-    }
+        var builder = WebApplication.CreateBuilder();
 
-    public T Divide<T>(T a, T b)
-    {
-        return (dynamic)a / (dynamic)b;
-    }
+        var app = builder.Build();
 
-    public T Mylt<T>(T a, T b)
-    {
-        return (dynamic)a * (dynamic)b;
-    }
+        app.UseStatusCodePagesWithReExecute("/error/{0}");
 
-    public T Subtract<T>(T a, T b)
-    {
-        return (dynamic)a - (dynamic)b;
-    }
-}
-
-interface ITimeService
-{
-    string GetTime();
-}
-
-class TimeService : ITimeService
-{
-    public string GetTime()
-    {
-        DateTime currentTime = DateTime.Now;
-
-        string timeOfDay;
-        if (currentTime.Hour >= 12 && currentTime.Hour < 18)
+        app.Map("/get-data", app1 => app1.Run(async context =>
         {
-            timeOfDay = "зараз день";
-        }
-        else if (currentTime.Hour >= 18 && currentTime.Hour < 24)
+            string format = "yyyy-MM-ddTHH:mm";
+            DateTime.TryParseExact(context.Request.Form["datetime"],
+                format,
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out DateTime resulted_date);
+            context.Response.Cookies.Append("name", context.Request.Form["value"],
+                new CookieOptions
+                {
+                    Expires = resulted_date.AddHours(3)
+                });
+            context.Response.Redirect("/");
+        }));
+        app.Map("/clear-data", app => app.Run(async context =>
         {
-            timeOfDay = "зараз вечір";
-        }
-        else if (currentTime.Hour >= 0 && currentTime.Hour < 6)
-        {
-            timeOfDay = "зараз ніч";
-        }
-        else
-        {
-            timeOfDay = "зараз ранок";
-        }
+            context.Response.Cookies.Delete("name");
+            context.Response.Redirect("/");
+        }));
 
-        return timeOfDay;
+        app.Run(async context =>
+        {
+            StringBuilder sb = new StringBuilder();
+            app.Logger.LogInformation("2");
+            context.Response.ContentType = "text/html; charset=utf-8";
+            if (context.Request.Cookies.TryGetValue("name", out var name))
+            {
+                sb.Clear();
+                sb.Append($"Hello {name}!\r\n");
+                sb.Append($"<a href=\"clear-data\"><button action=\"clear-data\" method=\"post\" type=\"button\"> Clear data </a>");
+                await context.Response.WriteAsync(sb.ToString());
+            }
+            else if (context.Request.Path != "/get-data" || context.Request.Path != "/clear-data")
+            {
+                sb.Append("<!DOCTYPE html>\r\n" +
+                    "<html>\r\n" +
+                    "<head>\r\n" +
+                    "    <title>Форма для введення значення та дати/часу</title>\r\n" +
+                    "</head>\r\n" +
+                    "<body>\r\n" +
+                    "    <h1>Форма для введення ім'я та дати/часу</h1>\r\n" +
+                    "    <form action=\"get-data\" method=\"post\">\r\n" +
+                    "        <label for=\"value\">ім'я:</label>\r\n" +
+                    "        <input type=\"text\" id=\"value\" name=\"value\" required><br><br>\r\n" +
+                    "        \r\n        <label for=\"datetime\">Дата і час:</label>\r\n" +
+                    "        <input type=\"datetime-local\" id=\"datetime\" name=\"datetime\" ><br><br>\r\n" +
+                    "        \r\n        <input type=\"submit\" value=\"Відправити\">\r\n" +
+                    "    </form>\r\n" +
+                    "</body>\r\n" +
+                    "</html>");
+                await context.Response.WriteAsync(sb.ToString());
+            }
+        });
+
+
+        app.Run();
     }
 }
